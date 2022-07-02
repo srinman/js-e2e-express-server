8f9138b7aa6730b9479fc86af5532319ffbb4047
Sridher Manivel <snmanivel@gmail.com>
2022-07-02 06:53:05 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ const port = process.env.PORT || 3000;
 server.create()
     .then(app => {
         app.listen(port, () => {
-            console.log(`Server has started on port ${port}!`);
+            console.log(`Server has really started on port ${port}!`);
         });
     }).catch(err => console.log(err));
